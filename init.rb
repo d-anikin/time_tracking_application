323e537c6ffd4be7bb30a293fe6d4d40e03e400a
Dmitry A <mr.ads@yandex.ru>
2016-09-11 07:50:12 +0300

    first

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :time_tracking_application do
+  name 'Time Tracking Application plugin'
+  author 'Dmitrii Anikin'
+  description 'Time Tracking Application'
+  version '0.0.1'
+  url 'http://www.redmine.org/plugins/time_tracking_application'
+  author_url 'https://github.com/mrads'
+end
