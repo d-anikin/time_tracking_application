4e0b252f3294e85206c96abb10ff4d6a5319f754
Dmitry A <mr.ads@yandex.ru>
2016-10-28 09:20:46 +0300

    fix

--- app/controllers/tta_issues_controller.rb
+++ app/controllers/tta_issues_controller.rb
@@ -8,20 +8,25 @@ class TtaIssuesController < ApplicationController
     assigned_ids = []
     assigned_ids << nil if Setting.plugin_time_tracking_application['unassigned'].to_i > 0
     assigned_ids << User.current.id if Setting.plugin_time_tracking_application['assigned_to_me'].to_i > 0
-    if Redmine::Plugin.registered_plugins[:redmine_backlog]
-      backlog = Backlog.query_backlog
-      backlog = backlog.where("(issues.assigned_to_id IN (?) OR issues.status_id = ?)" ,
-                              assigned_ids,
-                              Setting.plugin_time_tracking_application['test_status'].to_i)
-      backlog = backlog.includes(issue: [:assigned_to, :author, :status])
-      @issues = backlog.map(&:issue).select(&:visible?)
-    else
-      @issues = Issue.visible.open
-      @issues = @issues.joins(:priority).order("#{IssuePriority.table_name}.position DESC") if Setting.plugin_time_tracking_application['sort_by_priority'].to_i > 0
-      @issues = @issues.where(assigned_to_id: assigned_ids) if assigned_ids.any?
-      @issues = @issues.includes(:assigned_to, :author, :status)
-      @issues = @issues.limit(100)
-    end
+    # if Redmine::Plugin.registered_plugins[:redmine_backlog]
+    #   backlog = Backlog.query_backlog
+    #   backlog = backlog.where("(issues.assigned_to_id IN (?) OR issues.status_id = ?)" ,
+    #                           assigned_ids,
+    #                           Setting.plugin_time_tracking_application['test_status'].to_i)
+    #   backlog = backlog.includes(issue: [:assigned_to, :author, :status])
+    #   @issues = backlog.map(&:issue).select(&:visible?)
+    # else
+    #   @issues = Issue.visible.open
+    #   @issues = @issues.joins(:priority).order("#{IssuePriority.table_name}.position DESC") if Setting.plugin_time_tracking_application['sort_by_priority'].to_i > 0
+    #   @issues = @issues.where(assigned_to_id: assigned_ids) if assigned_ids.any?
+    #   @issues = @issues.includes(:assigned_to, :author, :status)
+    #   @issues = @issues.limit(100)
+    # end
+    @issues = Issue.visible.open
+    @issues = @issues.joins(:priority).order("#{IssuePriority.table_name}.position DESC") if Setting.plugin_time_tracking_application['sort_by_priority'].to_i > 0
+    @issues = @issues.where(assigned_to_id: assigned_ids) if assigned_ids.any?
+    @issues = @issues.includes(:assigned_to, :author, :status)
+    @issues = @issues.limit(100)
 
     respond_to do |format|
       format.api {}
